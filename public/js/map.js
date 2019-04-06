632bfeada342b52b3f1bf27bd41beb7203b7a213
adventuroussheep <jacob_haas_7@hotmail.com>
2019-04-06 11:37:27 -0700

    updated map

--- public/js/map.js
+++ public/js/map.js
@@ -1,118 +1,157 @@
 $(document).ready(function() {
-    // MapBox access token
+  // MapBox access token
   mapboxgl.accessToken =
     "pk.eyJ1IjoiNm4yciIsImEiOiJjanR2dTVrcHgxczl6NDVwaWhsZnZmYmluIn0.IUrjr1At8xiyqHacqoV6fg";
 
-    // To remove marker???
-//   if (!("remove" in Element.prototype)) {
-//     Element.prototype.remove = function() {
-//       if (this.parentNode) {
-//         this.parentNode.removeChild(this);
-//       }
-//     };
-//   }
 
-  //   Initial map display
-  var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/dark-v10",
-    center: [-77.034084142948, 38.909671288923],
-    zoom: 13,
-    scrollZoom: true
-  });
+    // Inital map display, removes MapBox JS errors
+    var map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [-77, 30],
+      zoom: 13,
+      scrollZoom: true
+    });
 
-//   List of users
+  //   Users object, used to generate map points and properties
   var users = {
     type: "FeatureCollection",
     features: [
       {
         type: "Feature",
         geometry: {
-          type: "Point",
-          coordinates: [-77.034084142948, 38.909671288923]
+          // coordinates: [0,0]
         },
         properties: {
-            name: "Pianist",
-          phoneFormatted: "(202) 234-7336",
-          phone: "2022347336",
-          address: "1471 P St NW",
-          city: "Washington DC",
-          country: "United States",
-          crossStreet: "at 15th St NW",
-          postalCode: "20005",
-          state: "D.C.",
-          instrument: "piano"
-        }
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-77.149766, 38.980772]
-        },
-        properties: {
-            name: "Guitarist",
-          phoneFormatted: "(202) 507-8357",
-          phone: "2025078357",
-          address: "2221 I St NW",
-          city: "Washington DC",
-          country: "United States",
-          crossStreet: "at 22nd St NW",
-          postalCode: "20037",
-          state: "D.C.",
-          instrument: "guitar"
-        }
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-77.043929, 38.910525]
-        },
-        properties: {
-          name: "dog",
-          phoneFormatted: "(202) 387-9338",
-          phone: "2023879338",
-          address: "1512 Connecticut Ave NW",
-          city: "Washington DC",
-          country: "United States",
-          crossStreet: "at Dupont Circle",
-          postalCode: "20036",
-          state: "D.C.",
-          instrument: "vocal"
+
         }
       }
     ]
   };
 
-  // form Button / appends new pin
-  $("#formBtn").on("click", function() {
-    var userNameInput = $("#userName")
-      .val()
-      .trim();
-    var userAddressInput = $("#userAddress")
-      .val()
-      .trim();
-    var userAddressQuery = encodeURIComponent(userAddressInput.trim());
-    var cityName = $("#cityId")
-      .val()
-      .trim();
-    var stateName = $("#stateId")
-      .val()
-      .trim();
-    // console.log(userAddressQuery);
+  // Grabs API info for all users
+  $.ajax({
+    url: "/map/api",
+    type: "get",
+    success: function(result) {
+      // result[0] is current user
+      // result[1] is database users
+
+      // current user 
+      userFirstName = result[0].firstName;
+      userLastName = result[0].lastName;
+      userName = userFirstName +" "+ userLastName;
+      userAddress = result[0].address;
+      userCity = result[0].city;
+      userState = result[0].state;
+      userInstrument = result[0].instruments;
+
+      // database users
+      databaseLength = result[1].length;
+      console.log(databaseLength);
+      for (var i = 0; i < databaseLength; i++){
+
+      dbFirstName = result[1][i].firstName;
+      dbLastName = result[1][i].lastName;
+      dbName = dbFirstName +" "+ dbLastName;
+
+      dbAddress = result[1][i].address;
+      dbCity = result[1][i].city;
+      dbState = result[1][i].state;
+      
+      dbInstrument = result[1][i].instruments;
 
 
+      var newObject = {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          // coordinates: [dbLong, dbLat]
+        },
+        properties: {
+          name: dbName,
+          phoneFormatted: "(202) 507-8357",
+          phone: "2025078357",
+          address: dbAddress,
+          city: dbCity,
+          country: "United States",
+          state: dbState,
+          instrument: dbInstrument
+        }
+      };
+      // pushes db users into user object
+      users.features.push(newObject);
+      
+      
+      
+      
+    }
+    console.log(users);
+  }
+  }).then(function(){
 
+    
+    
+    
+    // Calculates database users latitude and longitude
+    const dbLatLongUrl =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    dbAddress +
+    "%20" +
+    dbCity +
+    "%20" +
+    dbState +
+    ".json?access_token=" +
+    mapboxgl.accessToken +
+    "&cachebuster=1554258551887&autocomplete=true";
+    
+    
+    // Calculates the lat long
+    $.ajax({
+      url: dbLatLongUrl,
+      type: "get",
+      success: function(result) {
+        dbLong = result.features[0].center[0];
+        dbLat = result.features[0].center[1];
+        
+        var newObject = {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [dbLong, dbLat]
+          },
+          properties: {
+            name: dbName,
+            phoneFormatted: "(202) 507-8357",
+            phone: "2025078357",
+            address: dbAddress,
+            city: dbCity,
+            country: "United States",
+            state: dbState,
+            instrument: dbInstrument
+          }
+        };
+        // pushes the db user with lat and long
+        users.features.push(newObject);
+        console.log(users);
+        
+        
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    
+    });
 
-    // Converts address into lat and long cords
+}).then(function() {
+    // Converts current users address into lat and long cords
     const latLongUrl =
       "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-      userAddressQuery +
+      userAddress +
       "%20" +
-      cityName +
+      userCity +
       "%20" +
-      stateName +
+      userState +
       ".json?access_token=" +
       mapboxgl.accessToken +
       "&cachebuster=1554258551887&autocomplete=true";
@@ -121,7 +160,6 @@ $(document).ready(function() {
       url: latLongUrl,
       type: "get",
       success: function(result) {
-        console.log(result);
         userLong = result.features[0].center[0];
         userLat = result.features[0].center[1];
         // console.log(userLong);
@@ -129,188 +167,129 @@ $(document).ready(function() {
       error: function(err) {
         console.log(err);
       }
-
-
-
-
+      
       // Waits until lat and long are calculated
-      // Pushes new object into musicians array
-    }).then(function() {
-      var newObject = {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [userLong, userLat]
-        },
-        properties: {
-          name: userNameInput,
-          phoneFormatted: "(202) 507-8357",
-          phone: "2025078357",
-          address: userAddressInput,
-          city: cityName,
-          country: "United States",
-          crossStreet: "at 22nd St NW",
-          postalCode: "20037",
-          state: stateName,
-          instrument: "guitar"
-        }
-      };
-    //   Appends new user, runs pin display function
-      users.features.push(newObject);
+      // Sets map zoom to current user cords
+     
+    }).then(function(result) {
+      map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/dark-v10",
+        center: [userLong, userLat],
+        zoom: 13,
+        scrollZoom: true
+      });
+      });
+
+      
+      
+      // Displays pins
+      var mapPins = function() {
+        users.features.forEach(function(marker, i) {
+          var el = document.createElement("div"); // Create an img element for the marker
+          el.id = "marker-" + i;
+          el.className = "marker";
+          // Add markers to the map at all points
+          new mapboxgl.Marker(el, { offset: [0, -23] })
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+      
+
+        el.addEventListener("click", function(e) {
+          flyToStore(marker); // Fly to the point
+          createPopUp(marker); // Close all other popups and display popup for clicked store
+          var activeItem = document.getElementsByClassName("active");
+        })
+
+
+        $(".marker").css(
+          "background-image",
+          "url(../images/pianoPin.png)"
+          );
+          
+            // Changes pin image based on instrument
+            for (var i = 0; i < users.features.length-1; i++) {
+              if ($("#marker-" + i).css("background-image") == "none") {
+            if (users.features[i].properties.instrument == "piano") {
+              $("#marker-" + i).css(
+                "background-image",
+                "url(../images/pianoPin.png)"
+                );
+              }
+              if (users.features[i].properties.instrument == "guitar") {
+                $("#marker-" + i).css(
+                "background-image",
+                "url(../images/guitarPin.png)"
+                );
+              }
+              if (users.features[i].properties.instrument == "vocal") {
+                $("#marker-" + i).css(
+                  "background-image",
+                "url(../images/vocalPin.png)"
+                );
+              }
+              if (users.features[i].properties.instrument == "drums") {
+                $("#marker-" + i).css(
+                  "background-image",
+                  "url(../images/drumsPin.png)"
+                  );
+                }
+                if (users.features[i].properties.instrument == "brass") {
+                  $("#marker-" + i).css(
+                    "background-image",
+                    "url(../images/brassPin.png)"
+              );
+            }
+            if (users.features[i].properties.instrument == "woodwind") {
+              $("#marker-" + i).css(
+                "background-image",
+                "url(../../assets/images/woodwindPin.png)"
+                );
+              }
+            }
+          }
+        });
+      }; 
       mapPins();
     });
 
 
-
-    // Geocoder
-    // Converts address into longitude and latitude cords
-    const geoUrl =
-      "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-      cityName +
-      "%2C%20" +
-      stateName +
-      "%2C%20United%20States.json?access_token=" +
-      mapboxgl.accessToken +
-      "&cachebuster=1554243021563&autocomplete=true";
-
-    $.ajax({
-      url: geoUrl,
-      type: "get",
-      success: function(result) {
-        console.log(result);
-        longitude = result.features[0].bbox[0];
-        latitude = result.features[0].bbox[1];
-        // console.log(latitude);
-      },
-      error: function(err) {
-        console.log(err);
-      }
-
-
-      //   Directions and distances
-    //   Waits until address is converted
-    }).then(function() {
-      const distUrl =
-        "https://api.mapbox.com/directions/v5/mapbox/driving/-73.989%2C40.733%3B" +
-        longitude +
-        "%2C" +
-        latitude +
-        ".json?access_token=" +
-        mapboxgl.accessToken;
-
-      $.ajax({
-        url: distUrl,
-        type: "get",
-        success: function(result) {
-          console.log(result);
-        },
-        error: function(err) {
-          console.log(err);
-        }
+    
+    
+    
+    
+    // Centers camera to clicked pin
+    function flyToStore(currentFeature) {
+      map.flyTo({
+        center: currentFeature.geometry.coordinates,
+        zoom: 15
       });
-    });
-  });
-//   End of button click functions
-
-
-
-  // Displays pins
-  var mapPins = function() {
-    users.features.forEach(function(marker, i) {
-      var el = document.createElement("div"); // Create an img element for the marker
-      el.id = "marker-" + i;
-      el.className = "marker";
-      // Add markers to the map at all points
-      new mapboxgl.Marker(el, { offset: [0, -23] })
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
-
-      el.addEventListener("click", function(e) {
-        flyToStore(marker); // Fly to the point
-        createPopUp(marker); // Close all other popups and display popup for clicked store
-        var activeItem = document.getElementsByClassName("active");
-      });
-
-
-      // Changes pin image based on instrument
-      for (var i = 0; i < users.features.length; i++) {
-
-        if ($("#marker-" + i).css("background-image") == "none") {
-          if (users.features[i].properties.instrument == "piano") {
-            $("#marker-" + i).css(
-              "background-image",
-              "url(../assets/images/pianoPin.png)"           
-            );
-          }
-          if (users.features[i].properties.instrument == "guitar") {
-            $("#marker-" + i).css(
-              "background-image",
-              "url(./../../assets/images/guitarPin.png)"
-            );
-          }
-          if (users.features[i].properties.instrument == "vocal") {
-            $("#marker-" + i).css(
-              "background-image",
-              "url(../../assets/images/vocalPin.png)"
-            );
-        }
-        if (users.features[i].properties.instrument == "drums") {
-          $("#marker-" + i).css(
-            "background-image",
-            "url(../../assets/images/drumsPin.png)"
-          );
-        }
-        if (users.features[i].properties.instrument == "brass") {
-          $("#marker-" + i).css(
-            "background-image",
-            "url(../../assets/images/brassPin.png)"
-          );
-        }
-        if (users.features[i].properties.instrument == "woodwind") {
-          $("#marker-" + i).css(
-            "background-image",
-            "url(../../assets/images/woodwindPin.png)"
-          );
-          }
-        }
-      }
-    });
-  };
-  mapPins();
-
-
-
-  // Centers camera to clicked pin
-  function flyToStore(currentFeature) {
-    map.flyTo({
-      center: currentFeature.geometry.coordinates,
-      zoom: 15
-    });
-  }
-
-  // pop up for name/address
-  function createPopUp(currentFeature) {
-    var popUps = document.getElementsByClassName("mapboxgl-popup");
-    if (popUps[0]) popUps[0].remove();
-
-    var popup = new mapboxgl.Popup({ closeOnClick: false })
+      console.log(currentFeature);
+    }
+    
+    // pop up for name/address
+    function createPopUp(currentFeature) {
+      var popUps = document.getElementsByClassName("mapboxgl-popup");
+      if (popUps[0]) popUps[0].remove();
+      
+      var popup = new mapboxgl.Popup({ closeOnClick: false })
       .setLngLat(currentFeature.geometry.coordinates)
       .setHTML(
         "<h3>" +
-          currentFeature.properties.name +
-          "</h3>" +
-          "<h4>" +
-          currentFeature.properties.address +
-          "</h4>"
-      )
-      .addTo(map);
-  }
-
-  // Renders list to page
-  function buildLocationList(data) {
-    for (i = 0; i < data.features.length; i++) {
-      var currentFeature = data.features[i];
-      var prop = currentFeature.properties;
-    }
-  }
-});
+        currentFeature.properties.name +
+        "</h3>" +
+        "<h4>" +
+        currentFeature.properties.address +
+        "</h4>"
+        )
+        .addTo(map);
+      }
+      
+      // Renders list to page
+      function buildLocationList(data) {
+        for (i = 0; i < data.features.length; i++) {
+          var currentFeature = data.features[i];
+          var prop = currentFeature.properties;
+        }
+      }
+    });
